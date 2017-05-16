1efb7107f36162d772f33235a57d93d4794463c2
Akanksha2511 <21447915@student.uwa.edu.au>
2017-05-17 00:29:42 +0800

    updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'HOME',
-    version = '0.1',
+    version = '0.2',
     description="HOME: Histogram Of MEthylation",
     author = 'akanksha srivastava',
     install_requires = [
